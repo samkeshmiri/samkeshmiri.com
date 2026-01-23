5972756fe57481f10edd808c20f838dfeaa6a382
Sam Keshmiri <samkeshmiri94@gmail.com>
2026-01-23 22:19:26 +0000

    update bio

---
+++ src/components/Bio.js
@@ -0,0 +1,20 @@
+const Bio = () => {
+  return (
+    <section className="Bio">
+      <h1>Hey, I'm Sam!</h1>
+      <p>
+        <br></br>I'm building the future of finance with{" "}
+        <a
+          className="styled-link"
+          href="https://avara.xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aave Labs
+        </a>{" "}
+      </p>
+    </section>
+  );
+};
+
+export default Bio;
