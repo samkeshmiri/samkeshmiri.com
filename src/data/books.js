b32318ae0ec058775bf5850d205899c81b4f1932
Sam Keshmiri <samkeshmiri94@gmail.com>
2026-03-22 17:57:14 +0000

    update book list

--- src/data/books.js
+++ src/data/books.js
@@ -2,13 +2,391 @@
 // The UI reverses the array so the latest addition appears first.
 export const books = [
   {
+    title: "The Pragmatic Programmer",
+    author: "David Thomas and Andrew Hunt",
+    year: "1999",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Pragmatic_Programmer",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/8/8f/The_pragmatic_programmer.jpg",
+    coverImageWidth: 282,
+    coverImageHeight: 354
+  },
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    year: "2008",
+    wikiUrl: "https://en.wikipedia.org/wiki/Robert_C._Martin",
+    coverImageUrl: "https://books.google.com/books/content?id=hjEFCAAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+  },
+  {
+    title: "Clean Architecture",
+    author: "Robert C. Martin",
+    year: "2017",
+    wikiUrl: "https://en.wikipedia.org/wiki/Robert_C._Martin",
+    coverImageUrl: "https://books.google.com/books/content?id=uGE1DwAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+  },
+  {
+    title: "HTTP: The Definitive Guide",
+    author: "David Gourley and Brian Totty",
+    year: "2002",
+    coverImageUrl: "https://books.google.com/books/content?id=3EybAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "Designing Data-Intensive Applications",
+    author: "Martin Kleppmann",
+    year: "2017",
+    coverImageUrl: "https://books.google.com/books/content?id=p1heDgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "Mastering Ethereum",
+    author: "Andreas M. Antonopoulos and Gavin Wood",
+    year: "2018",
+    wikiUrl: "https://en.wikipedia.org/wiki/Andreas_Antonopoulos",
+    coverImageUrl: "https://books.google.com/books/content?id=oJJ5DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "Read Write Own",
+    author: "Chris Dixon",
+    year: "2024",
+    wikiUrl: "https://en.wikipedia.org/wiki/Chris_Dixon",
+    coverImageUrl: "https://books.google.com/books/content?id=HWTOEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "DeFi and the Future of Finance",
+    author: "Campbell R. Harvey, Ashwin Ramachandran, and Joey Santoro",
+    year: "2021",
+    wikiUrl: "https://en.wikipedia.org/wiki/Campbell_Harvey",
+    coverImageUrl: "https://books.google.com/books/content?id=YCY_EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "Building Microservices",
+    author: "Sam Newman",
+    year: "2015",
+    coverImageUrl: "https://books.google.com/books/content?id=ZvM5EAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "A Common-Sense Guide to Data Structures and Algorithms",
+    author: "Jay Wengrow",
+    year: "2017",
+    coverImageUrl: "https://books.google.com/books/content?id=DMxYEQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "Head First Design Patterns",
+    author: "Eric Freeman et al.",
+    year: "2004",
+    wikiUrl: "https://en.wikipedia.org/wiki/Eric_Freeman",
+    coverImageUrl: "https://books.google.com/books/content?id=DD1OtAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+  },
+  {
+    title: "Superforecasting",
+    author: "Philip E. Tetlock and Dan Gardner",
+    year: "2015",
+    wikiUrl: "https://en.wikipedia.org/wiki/Philip_E._Tetlock",
+    coverImageUrl: "https://books.google.com/books/content?id=_lMPDAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "A Lust for Window Sills",
+    author: "Harry Mount",
+    year: "2008",
+    wikiUrl: "https://en.wikipedia.org/wiki/Harry_Mount",
+    coverImageUrl: "https://books.google.com/books/content?id=EXw6EQAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+  },
+  {
+    title: "No Logo",
+    author: "Naomi Klein",
+    year: "1999",
+    wikiUrl: "https://en.wikipedia.org/wiki/No_Logo",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/No_Logo_cover.jpg/330px-No_Logo_cover.jpg",
+    coverImageWidth: 338,
+    coverImageHeight: 450
+  },
+  {
+    title: "The Shah",
+    author: "Abbas Milani",
+    year: "2011",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Shah_(book)",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/9/9f/The_Shah_%28book%29.jpg",
+    coverImageWidth: 258,
+    coverImageHeight: 392
+  },
+  {
+    title: "Hitler 1936-1945: Nemesis",
+    author: "Ian Kershaw",
+    year: "2008",
+    wikiUrl: "https://en.wikipedia.org/wiki/Ian_Kershaw",
+    coverImageUrl: "https://books.google.com/books/content?id=l5TvVKVZvPoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "Hitler 1889-1936: Hubris",
+    author: "Ian Kershaw",
+    year: "2008",
+    wikiUrl: "https://en.wikipedia.org/wiki/Ian_Kershaw",
+    coverImageUrl: "https://books.google.com/books/content?id=nV-N10gyoFwC&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+  },
+  {
+    title: "The Inevitable Americans",
+    author: "John Greenway",
+    year: "1964",
+    wikiUrl: "https://en.wikipedia.org/wiki/John_Greenway_(folklorist)",
+    coverImageUrl: "https://books.google.com/books/content?id=w3AiAAAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+  },
+  {
+    title: "Ten Times Calmer",
+    author: "Kirren Schnack",
+    year: "2024",
+    coverImageUrl: "https://books.google.com/books/content?id=UzWHEAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "59 Seconds",
+    author: "Richard Wiseman",
+    year: "2009",
+    wikiUrl: "https://en.wikipedia.org/wiki/Richard_Wiseman",
+    coverImageUrl: "https://books.google.com/books/content?id=QDnzzwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+  },
+  {
+    title: "Ogilvy on Advertising",
+    author: "David Ogilvy",
+    year: "1983",
+    wikiUrl: "https://en.wikipedia.org/wiki/David_Ogilvy_(businessman)",
+    coverImageUrl: "https://books.google.com/books/content?id=67FPAAAAMAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+  },
+  {
+    title: "Watchmen",
+    author: "Alan Moore",
+    year: "1987",
+    wikiUrl: "https://en.wikipedia.org/wiki/Watchmen",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/a/a2/Watchmen%2C_issue_1.jpg",
+    coverImageWidth: 253,
+    coverImageHeight: 394
+  },
+  {
+    title: "Maus",
+    author: "Art Spiegelman",
+    year: "1991",
+    wikiUrl: "https://en.wikipedia.org/wiki/Maus",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/7/7d/Maus_%28volume_1%29_cover.jpg",
+    coverImageWidth: 267,
+    coverImageHeight: 372
+  },
+  {
+    title: "The Three-Body Problem",
+    author: "Liu Cixin",
+    year: "2006",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Three-Body_Problem_(novel)",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/0/0f/Threebody.jpg",
+    coverImageWidth: 263,
+    coverImageHeight: 380
+  },
+  {
+    title: "The Dark Forest",
+    author: "Liu Cixin",
+    year: "2008",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Dark_Forest",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/2/2c/TheDarkForestNovelCover.jpg",
+    coverImageWidth: 259,
+    coverImageHeight: 385
+  },
+  {
+    title: "Death's End",
+    author: "Liu Cixin",
+    year: "2010",
+    wikiUrl: "https://en.wikipedia.org/wiki/Death's_End",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/6/62/Death%27s_End_-_bookcover.jpg",
+    coverImageWidth: 257,
+    coverImageHeight: 386
+  },
+  {
+    title: "Dune",
+    author: "Frank Herbert",
+    year: "1965",
+    wikiUrl: "https://en.wikipedia.org/wiki/Dune_(novel)",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/d/de/Dune-Frank_Herbert_%281965%29_First_edition.jpg",
+    coverImageWidth: 259,
+    coverImageHeight: 391
+  },
+  {
+    title: "Dune Messiah",
+    author: "Frank Herbert",
+    year: "1969",
+    wikiUrl: "https://en.wikipedia.org/wiki/Dune_Messiah",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Dune_Messiah_%281969%29_cover.jpg",
+    coverImageWidth: 3372,
+    coverImageHeight: 4904
+  },
+  {
+    title: "Do Androids Dream of Electric Sheep?",
+    author: "Philip K. Dick",
+    year: "1968",
+    wikiUrl: "https://en.wikipedia.org/wiki/Do_Androids_Dream_of_Electric_Sheep%3F",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/DoAndroidsDream.png/330px-DoAndroidsDream.png",
+    coverImageWidth: 483,
+    coverImageHeight: 750
+  },
+  {
+    title: "The City and the Stars",
+    author: "Arthur C. Clarke",
+    year: "1956",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_City_and_the_Stars",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/f/f9/The_City_and_the_Stars_hardcover.jpg",
+    coverImageWidth: 257,
+    coverImageHeight: 382
+  },
+  {
+    title: "Flowers for Algernon",
+    author: "Daniel Keyes",
+    year: "1966",
+    wikiUrl: "https://en.wikipedia.org/wiki/Flowers_for_Algernon",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/e/ea/FlowersForAlgernon.jpg",
+    coverImageWidth: 263,
+    coverImageHeight: 379
+  },
+  {
+    title: "The Martian Way",
+    author: "Isaac Asimov",
+    year: "1955",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Martian_Way_and_Other_Stories",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/6/60/Martian_way.jpg",
+    coverImageWidth: 261,
+    coverImageHeight: 389
+  },
+  {
+    title: "Foundation",
+    author: "Isaac Asimov",
+    year: "1951",
+    wikiUrl: "https://en.wikipedia.org/wiki/Foundation_(Asimov_novel)",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/d/d9/Foundation_-_Isaac_Asimov_%28Gnome_1951%29.jpg",
+    coverImageWidth: 245,
+    coverImageHeight: 360
+  },
+  {
+    title: "The Man in the High Castle",
+    author: "Philip K. Dick",
+    year: "1962",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Man_in_the_High_Castle",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a6/The_Man_in_the_High_Castle_%281962%29.jpg",
+    coverImageWidth: 2750,
+    coverImageHeight: 4056
+  },
+  {
+    title: "The Yellow Wallpaper",
+    author: "Charlotte Perkins Gilman",
+    year: "1892",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Yellow_Wallpaper",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/40/The_Yellow_Wall_Paper_pg_1.jpg",
+    coverImageWidth: 2627,
+    coverImageHeight: 3959
+  },
+  {
+    title: "The Wisdom of Insecurity",
+    author: "Alan Watts",
+    year: "1951",
+    wikiUrl: "https://en.wikipedia.org/wiki/Alan_Watts",
+    coverImageUrl: "https://books.google.com/books/content?id=dUDGGSd8QSEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "The Metamorphosis",
+    author: "Franz Kafka",
+    year: "1915",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Metamorphosis",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Franz_Kafka_Die_Verwandlung_1916_Orig.-Pappband.jpg/500px-Franz_Kafka_Die_Verwandlung_1916_Orig.-Pappband.jpg",
+    coverImageWidth: 920,
+    coverImageHeight: 1480
+  },
+  {
+    title: "Jonathan Livingston Seagull",
+    author: "Richard Bach",
+    year: "1970",
+    wikiUrl: "https://en.wikipedia.org/wiki/Jonathan_Livingston_Seagull",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/7/7b/Johnathan_Livingston_Seagull.jpg",
+    coverImageWidth: 280,
+    coverImageHeight: 365
+  },
+  {
+    title: "The Periodic Table",
+    author: "Primo Levi",
+    year: "1975",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Periodic_Table_(short_story_collection)",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/b/b9/Periodic_table.jpg",
+    coverImageWidth: 251,
+    coverImageHeight: 397
+  },
+  {
+    title: "W, or the Memory of Childhood",
+    author: "Georges Perec",
+    year: "1975",
+    wikiUrl: "https://en.wikipedia.org/wiki/W%2C_or_the_Memory_of_Childhood",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/e/e8/W%2C_or_the_Memory_of_Childhood.jpg",
+    coverImageWidth: 210,
+    coverImageHeight: 370
+  },
+  {
+    title: "The Doors of Perception",
+    author: "Aldous Huxley",
+    year: "1954",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Doors_of_Perception",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/1/17/DoorsofPerception.jpg",
+    coverImageWidth: 262,
+    coverImageHeight: 380
+  },
+  {
+    title: "Brave New World",
+    author: "Aldous Huxley",
+    year: "1932",
+    wikiUrl: "https://en.wikipedia.org/wiki/Brave_New_World",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/6/62/BraveNewWorld_FirstEdition.jpg",
+    coverImageWidth: 258,
+    coverImageHeight: 387
+  },
+  {
+    title: "Mother Night",
+    author: "Kurt Vonnegut",
+    year: "1961",
+    wikiUrl: "https://en.wikipedia.org/wiki/Mother_Night",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/5/5c/MotherNight%28Vonnegut%29.jpg",
+    coverImageWidth: 242,
+    coverImageHeight: 412
+  },
+  {
+    title: "Slaughterhouse-Five",
+    author: "Kurt Vonnegut",
+    year: "1969",
+    wikiUrl: "https://en.wikipedia.org/wiki/Slaughterhouse-Five",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/ad/Slaughterhouse-Five_%28first_edition%29_-_Kurt_Vonnegut.jpg",
+    coverImageWidth: 1029,
+    coverImageHeight: 1500
+  },
+  {
+    title: "A Confederacy of Dunces",
+    author: "John Kennedy Toole",
+    year: "1980",
+    wikiUrl: "https://en.wikipedia.org/wiki/A_Confederacy_of_Dunces",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/6/67/Confederacy_of_dunces_cover.jpg",
+    coverImageWidth: 255,
+    coverImageHeight: 391
+  },
+  {
+    title: "The Conquest of Happiness",
+    author: "Bertrand Russell",
+    year: "1930",
+    wikiUrl: "https://en.wikipedia.org/wiki/Bertrand_Russell",
+    coverImageUrl: "https://books.google.com/books/content?id=ODIiumCiFOoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  },
+  {
+    title: "The Handmaid's Tale",
+    author: "Margaret Atwood",
+    year: "1985",
+    wikiUrl: "https://en.wikipedia.org/wiki/The_Handmaid's_Tale",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/1/18/TheHandmaidsTale%281stEd%29.jpg",
+    coverImageWidth: 250,
+    coverImageHeight: 381
+  },
+  {
     title: "Orbital",
     author: "Samantha Harvey",
     year: "2024",
     wikiUrl: "https://en.wikipedia.org/wiki/Orbital_%28novel%29",
     coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/8/8b/Orbital.jpg",
     coverImageWidth: 264,
-    coverImageHeight: 385,
+    coverImageHeight: 385
   },
   {
     title: "Tomorrow, and Tomorrow, and Tomorrow",
@@ -17,7 +395,7 @@ export const books = [
     wikiUrl: "https://en.wikipedia.org/wiki/Tomorrow%2C_and_Tomorrow%2C_and_Tomorrow_%28novel%29",
     coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/c/c4/Tomorrow%2C_and_Tomorrow%2C_and_Tomorrow.jpg",
     coverImageWidth: 256,
-    coverImageHeight: 390,
+    coverImageHeight: 390
   },
   {
     title: "The Fixer",
@@ -26,13 +404,21 @@ export const books = [
     wikiUrl: "https://en.wikipedia.org/wiki/The_Fixer_%28novel%29",
     coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/e/e3/Fixer.JPG",
     coverImageWidth: 256,
-    coverImageHeight: 390,
+    coverImageHeight: 390
   },
   {
     title: "One Day, Everyone Will Have Always Been Against This",
     author: "Omar El Akkad",
     year: "2025",
-    coverImageUrl:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/One_Day%2C_Everyone_Will_Have_Always_Been_Against_This_book_cover.jpg/250px-One_Day%2C_Everyone_Will_Have_Always_Been_Against_This_book_cover.jpg",
+    wikiUrl: "https://en.wikipedia.org/wiki/One_Day%2C_Everyone_Will_Have_Always_Been_Against_This",
+    coverImageUrl: "https://upload.wikimedia.org/wikipedia/en/9/9f/One_Day%2C_Everyone_Will_Have_Always_Been_Against_This_book_cover.jpg",
+    coverImageWidth: 257,
+    coverImageHeight: 388
   },
+  {
+    title: "Clubbing",
+    author: "Ben Malbon",
+    year: "1999",
+    coverImageUrl: "https://books.google.com/books/content?id=i_fHcomwelAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+  }
 ];
